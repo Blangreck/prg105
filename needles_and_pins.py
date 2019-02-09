a7b6e0ed74f06006d063e9f21115bf3864164a64
Blangreck <47305354+Blangreck@users.noreply.github.com>
2019-02-08 19:39:19 -0600

    Add files via upload

---
+++ needles_and_pins.py
@@ -0,0 +1,27 @@
+print("NEEDLES AND PINS \n")
+
+print("Needles and pins,")
+print("Needles and pins,")
+print("Sew me a sail")
+print("To catch me the wind.\n")
+
+print("Sew me a sail")
+print("Strong as the gale,")
+print("Carpenter, bring out your")
+print("Hammers and nails. \n")
+
+print("Hammers and nails,")
+print("Hammers and nails,")
+print("Build me a boat")
+print("To go chasing the whales. \n")
+
+print("Chasing the whales,")
+print("Sailing the blue,")
+print("Find me a captain")
+print("And sign me a crew. \n")
+
+print("Captain and crew,")
+print("Captain and crew,")
+print("Take me, oh take me")
+print("To anywhere new.")
+
