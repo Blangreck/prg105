f085cefce931b273c490f42a348b2aea24a429e3
Blangreck <47305354+Blangreck@users.noreply.github.com>
2019-02-12 12:47:24 -0600

    Add files via upload

---
+++ 2.2_madlibs.py
@@ -0,0 +1,38 @@
+# Variables for mad libs program
+sharp = input("Enter an object that is sharp: ")
+keen = input("Enter a keen object: ")
+sharp2 = input("Enter a sharp object: ")
+cloth = input("Enter something that can be sewed: ")
+wind = input("Enter a word that describes movement in the air: ")
+gale = input("Weather condition involving rain: ")
+person = input("Enter a type of job that uses hammers: ")
+animal = input("Enter an sea creature: ")
+leader = input("Enter a type of leader: ")
+# Printing the Mad libs results
+print("NEEDLES AND " + sharp + "\n")
+
+print("Needles and " + keen + ",")
+print("Needles and " + sharp + ",")
+print("Sew me a " + cloth + ",")
+print("To catch me the " + wind + "\n")
+
+print("Sew me a sail ")
+print("Strong as the " + gale + ",")
+print(person + ", bring out your ")
+print("Hammers and nails. \n")
+
+print("Hammers and nails, ")
+print("Hammers and nails, ")
+print("Build me a boat ")
+print("To go chasing the whales. \n")
+
+print("Chasing the " + animal + ",")
+print("Sailing the blue, ")
+print("Find me a " + leader + ",")
+print("And sign me a crew. \n")
+
+print("Captain and crew, ")
+print("Captain and crew, ")
+print("Take me, oh take me ")
+print("To anywhere new. ")
+
