453d027addf31377291b2ef96695399458e25928
Blangreck <47305354+Blangreck@users.noreply.github.com>
2019-02-25 10:35:39 -0600

    Add files via upload

---
+++ 4.2_pennies_for_pay.py
@@ -0,0 +1,12 @@
+days_worked = int(input("Please enter how many days you worked: "))
+total_number_of_pennies = 0
+
+print("Day\tSalary")
+for current_day in range(days_worked):
+    pennies_for_the_day = 2 ** current_day
+    total_number_of_pennies += pennies_for_the_day
+    print(current_day + 1, "\t", pennies_for_the_day)
+
+total_pay = total_number_of_pennies * 0.01
+
+print("\nTotal pay: $", total_pay)
